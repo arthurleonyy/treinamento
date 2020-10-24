45bf0ddb64d4452584992b27d0d0590bdfa5d302
Paulo Roque <paulinhoroque@hotmail.com>
2020-10-24 15:59:12 -0300

    resolvido o busca por email

--- controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
+++ controller/src/main/java/com/indracompany/treinamento/controller/rest/ClienteRest.java
@@ -3,16 +3,24 @@ package com.indracompany.treinamento.controller.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.indracompany.treinamento.model.entity.Cliente;
 import com.indracompany.treinamento.model.service.ClienteService;
+
+import io.micrometer.core.ipc.http.HttpSender.Request;
 
 
 @RestController()
@@ -24,24 +32,25 @@ public class ClienteRest extends GenericCrudRest<Cliente, Long, ClienteService>{
 	@Autowired
 	private ClienteService clienteService;
 	
-	@GetMapping("/{email}/")
-	public ResponseEntity<Cliente> getClienteByEmail(@PathVariable("email") String email){
+	
+	  @GetMapping("procurando/{email}/")
+	  @ResponseStatus(code = HttpStatus.FOUND)
+	  public  ResponseEntity<Cliente> getClienteByEmail(@PathVariable("email") String email){ 
+		 return ResponseEntity.ok(this.clienteService.buscarEmail(email)) ; 
 		
-		Cliente cliente = this.clienteService.buscarEmail(email);
-		return ResponseEntity.ok(cliente);
-	}
+	  }
+	 
 	
-	
-	
-	/*
-	 * @GetMapping("/{cpf}") public ResponseEntity<Cliente>
-	 * buscarCpf(@PathVariable("cpf") String cpf){
-	 * 
-	 * Cliente cliente = this.clienteService.buscarCpf(cpf); return
-	 * ResponseEntity.ok(cliente);
-	 * 
-	 * }
-	 */
+	  @GetMapping("/{cpf}") 
+	  @ResponseStatus(code = HttpStatus.FOUND)
+	  public Cliente buscarCpf(@PathVariable("cpf") String cpf){
+		  Cliente cliente = this.clienteService.buscarCpf(cpf); 
+	  
+		  return cliente;
+	  
+	 }
+	 
+	 
 	 
 	
 	
--- model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
+++ model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
@@ -1,5 +1,7 @@
 package com.indracompany.treinamento.model.service;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,27 +20,31 @@ public class ClienteService extends GenericCrudService<Cliente, Long, ClienteRep
 	public Cliente buscarEmail(String email){
 		
 		Cliente cliente  = cr.findByEmail(email);
-		 return cliente; 
+		
+		return cliente; 
 		
 	}
 	
 	
-	/*
-	 * public Cliente buscarCpf(String cpf) {
-	 * 
-	 * if(!cpfIsValido(cpf)) { throw new
-	 * AplicacaoException(ExceptionValidacoes.ERROR_CPF_INVALIDO); }
-	 * 
-	 * 
-	 * Cliente cliente = cr.findByCpf(cpf); return cliente;
-	 * 
-	 * 
-	 * }
-	 * 
-	 * 
-	 * 
-	 * private boolean cpfIsValido(String cpf) { return CpfUtils.validaCPF(cpf); }
-	 */
+	
+	
+	  public Cliente buscarCpf(String cpf) {
+	  
+	  //if(!cpfIsValido(cpf)) { 
+		  //throw new
+	 // AplicacaoException(ExceptionValidacoes.ERROR_CPF_INVALIDO); 
+	// }else {
+	  
+	  Cliente cliente = cr.findByCpf(cpf); 
+	  return cliente; //}
+	  
+	  }
+	  
+	  
+	  
+	  //private boolean cpfIsValido(String cpf) { return CpfUtils.validaCPF(cpf); }
+	 
+	 
 		 
 	 
 }
