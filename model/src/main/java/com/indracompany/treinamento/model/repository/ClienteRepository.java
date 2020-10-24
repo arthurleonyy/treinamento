d541c4979800a6b85f7da73af89aa0ff8da1ee78
CJR <juninn89@gmail.com>
2020-10-24 16:30:41 -0300

    Atualizado

--- model/src/main/java/com/indracompany/treinamento/model/repository/ClienteRepository.java
+++ model/src/main/java/com/indracompany/treinamento/model/repository/ClienteRepository.java
@@ -9,8 +9,6 @@ public interface ClienteRepository extends GenericCrudRepository<Cliente, Long> 
 	Cliente findByCpf(String cpf);
 	
 	List<Cliente> findByNomeContaining(String nome);	
-
-	Cliente findByNome(String nome);
 	
 
 }
