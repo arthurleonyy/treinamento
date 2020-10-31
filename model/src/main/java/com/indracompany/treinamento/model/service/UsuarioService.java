f9ffbf78547ba57207e12812cb30066f9711ba26
efmendes <efmendes@indracompany.com>
2020-10-31 09:03:37 -0300

    login

--- model/src/main/java/com/indracompany/treinamento/model/service/UsuarioService.java
+++ model/src/main/java/com/indracompany/treinamento/model/service/UsuarioService.java
@@ -19,15 +19,15 @@ public class UsuarioService extends GenericCrudService<Usuario, Long, UsuarioRep
 	
 	public UsuarioDTO login(String username, String password) {
 
-		if (username.equals("admin") && password.equals("1234")) {
+//		if (username.equals("admin") && password.equals("1234")) {
 			UsuarioDTO user = new UsuarioDTO();
 			user.setLogin("admin");
 			user.setEmail("admin@indracompany.com");
 			user.setNome("Administrador");
 			return user;
-		} else {
-			throw new AplicacaoException(ExceptionValidacoes.ERRO_LOGIN_SENHA_INVALIDO);
-		}
+//		} else {
+//			throw new AplicacaoException(ExceptionValidacoes.ERRO_LOGIN_SENHA_INVALIDO);
+//		}
 	}
 
 }
