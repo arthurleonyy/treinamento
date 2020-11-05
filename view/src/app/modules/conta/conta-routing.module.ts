87b5e09271000c16f72e290690e6ac86ac124e9c
Claudio Junior <Cjrnew@gmail.com>
2020-11-04 22:18:13 -0300

    Adicionado componente 'depositar' como filho de conta.

--- view/src/app/modules/conta/conta-routing.module.ts
+++ view/src/app/modules/conta/conta-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContaComponent } from './conta.component';
+import { DepositarComponent } from './depositar/depositar.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ContaComponent,
+    children : [
+      {path: 'depositar', component: DepositarComponent}
+    ]
   },
 ];
 
--- view/src/app/modules/conta/conta.module.ts
+++ view/src/app/modules/conta/conta.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContaComponent } from './conta.component';
 import { ContaRoutingModule } from './conta-routing.module';
+import { DepositarComponent } from './depositar/depositar.component';
 
 @NgModule({
   declarations: [
     ContaComponent,
+    DepositarComponent,
   ],
   imports: [
     CommonModule,
