394ee876be0d38e76ef99cf003aa6a7c58a33af5
Arthur Leony <arthur.santiago@sefaz.pb.gov.br>
2020-11-07 18:37:00 -0300

    Aula-02

--- view/src/app/modules/conta/conta-routing.module.ts
+++ view/src/app/modules/conta/conta-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ContaComponent } from './conta.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ContaComponent } from './pages/conta.component';
+import { DepositarSacarComponent } from './pages/depositar-sacar/depositar-sacar.component';
+import { OperacoesComponent } from './pages/operacoes/operacoes.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ContaComponent,
+    children: [
+      {
+        path: '',
+        component: OperacoesComponent
+      },
+      {
+        path: 'operacoes',
+        component: OperacoesComponent
+      },
+      {
+        path: 'depositar',
+        component: DepositarSacarComponent
+      },
+      {
+        path: 'sacar',
+        component: DepositarSacarComponent
+      },
+    ]
   },
 ];
 
