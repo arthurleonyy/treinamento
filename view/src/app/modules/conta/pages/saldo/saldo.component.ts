f3771188594e91b5e37ba700a5af4e25b6c663a3
Rodrigo Viana <rodrigoviana50@gmail.com>
2020-11-10 14:24:33 -0300

    Terminando de subir o saldo

---
+++ view/src/app/modules/conta/pages/saldo/saldo.component.ts
@@ -0,0 +1,83 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormBase } from 'src/app/core/classes/form-base';
+import { ContaSaldo } from 'src/app/core/models/conta.model';
+import { ContaService } from 'src/app/core/services/conta.service';
+import { SweetalertCustom } from 'src/app/shared/utils/sweetalert-custom';
+
+@Component({
+  selector: 'app-saldo',
+  templateUrl: './saldo.component.html',
+  styleUrls: ['./saldo.component.scss']
+})
+export class SaldoComponent extends FormBase implements OnInit, AfterViewInit {
+  
+  respostaSaldo = '';
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private contaService: ContaService,
+    public router: Router
+  ) {
+    super();
+  }
+
+  ngOnInit() {
+    this.createFormGroup();
+    this.validateMensageError();
+
+  }
+
+
+
+  createFormGroup() {
+    this.form = this.formBuilder.group({
+      agencia:      ['', Validators.required],
+      numeroConta:  ['', Validators.required],
+    });
+  }
+
+  /**
+   * Seta a mensagem de validação que irá ser exibida ao usuário
+   */
+  validateMensageError() {
+    this.createValidateFieldMessage({
+      agencia: {
+        required: 'Agência obrigatória.',
+      },
+      numeroConta: {
+        required: 'Número da conta obrigatório.',
+      },
+    });
+  }
+
+  onSubmit() {
+    
+    if (this.form.valid) {
+      
+      const contaSaldo = new ContaSaldo(this.form.value); {
+        this.saldo(contaSaldo);
+    }
+  }
+}
+
+  private saldo(contaSaldo: ContaSaldo) {
+
+    this.contaService.saldo(contaSaldo).subscribe(
+      response => {
+        this.respostaSaldo = response.body;
+      },
+      
+      erro => {
+        if (erro.error.detalhes) {
+          SweetalertCustom.showAlertConfirm(erro.error.detalhes[0], { type: 'error' });
+        } else {
+          SweetalertCustom.showAlertConfirm('Falha na operação.', { type: 'error' });
+        }
+      }
+    );
+  }
+
+}
